181e03c92e9a6d710d75363cf409bd1d9939f225
hachihao792001 <hachihao792001@gmail.com>
2026-04-12 16:15:03 +0700

    make the blogs page looks better

--- js/blogs.js
+++ js/blogs.js
@@ -9,31 +9,17 @@ function renderBlogs(data, container) {
     data.forEach(item => {
         const blogItem = document.createElement('div');
         blogItem.className = 'blogs-item-container';
-
-        const hasChildren = item.children && item.children.length > 0;
         
         blogItem.innerHTML = `
-            <div class="blogs-item">
-                ${hasChildren ? '<span class="arrow">▶</span>' : ''}
-                <a href="${item.route}">${item.title}</a>
-                <p>${item.date}</p>
+            <div class="blogs-item${item.isSeries ? ' blogs-item-series' : ''}">
+                <div class="blogs-item-title">
+                    ${item.isSeries ? '<span class="series-badge">Series</span>' : ''}
+                    <a href="${item.route}">${item.title}</a>
+                </div>
+                ${item.date ? `<span class="blogs-item-date">${item.date}</span>` : ''}
                 ${getCommingSoonHTML(item.isWritten)}
             </div>
-            ${hasChildren ? `<div class="children-container" style="display: none; margin-left: 20px;"></div>` : ''}
         `;
-
-        if (hasChildren) {
-            const childTarget = blogItem.querySelector('.children-container');
-            const arrow = blogItem.querySelector('.arrow');
-            
-            renderBlogs(item.children, childTarget);
-
-            arrow.addEventListener('click', (e) => {
-                const isExpanded = childTarget.style.display === 'block';
-                childTarget.style.display = isExpanded ? 'none' : 'block';
-                arrow.classList.toggle('arrow-down', !isExpanded);
-            });
-        }
 
         container.appendChild(blogItem);
     });
